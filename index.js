73131968c7da15f46fdbe6005cd37ce159fb1b5d
Raphael <raphael.mazet@gmail.com>
2021-01-19 17:51:42 +0000

    create dev environment

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
-const path = require('path')
 
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3020;
 
 const corsConfig = {
-  origin: 'http://localhost:3000',
+  origin: process.env.NODE_ENV === 'production' ? 'https://simplefi.finance' : 'http://localhost:3000',
   credentials: true,
 };
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(router);
 
 app.listen(port, () => {
+  console.log(' ---> process.env.NODE_ENV', process.env.NODE_ENV);
   console.log(`SimpleFi server listening on localhost:${port} 🎉`)
 });
